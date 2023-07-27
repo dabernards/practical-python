f2cf3f19ed325e2b12eea832af38c09741775b2b
Dan Bernards <github@dbernards.com>
2023-07-27 10:24:43 -0700

    Completed Lesson 1.4 - Strings (minus refactoring mortgage.py

---
+++ Work/strings.py
@@ -0,0 +1,16 @@
+# 1.13 - run through
+
+# 1.14
+symbols = 'AAPL,IBM,MSFT,YHOO,SCO'
+# need to add comma in
+symbols += ',GOOG'
+# or ugly way
+# symbols = ','.join([*symbols.split(','), 'GOOG'])
+
+
+symbols = 'HPQ,' + symbols
+
+# 1.15/16/17 run through
+# Need to revisit mortgage.py that's at UCSF
+
+#1.18 regex overview -- all makes sense, don't know this correct characters though
