a591c21c231354a4f34e35b83f23c63d5982261c
Dan Bernards <dabernards@gmail.com>
2023-07-31 14:05:12 -0700

    Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f).strip().split(',')
+    data = f.read().strip()
+cost = 0
+
+for item in data.split('\n'):
+    _, shares, price = item.split(',')
+    cost += int(shares)*float(price)
+
+print(f'Total cost {cost}')
+
+# import gzip
+# with gzip.open('Data/portfolio.csv.gz', 'rt') as f:
+#     for line in f:
+#         print(line, end="")
