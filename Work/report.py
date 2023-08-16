21454f6dd08b7b8aaafb808c694d74f1d5200ff5
Dan Bernards <dabernards@gmail.com>
2023-08-16 12:30:01 -0700

    Code cleanup following parse_csv incorporation

--- Work/report.py
+++ Work/report.py
@@ -20,44 +20,12 @@ def portfolio_cost(filename):
 
 def read_portfolio(filename):
     ''' Read a portfolio csv file and returns data as a list of tuples '''
-
-    # portfolio = []
-    # with open(filename, 'rt') as f:
-    #     rows = csv.reader(f)
-    #     headers = next(rows)
-    #     for row in rows:
-    #         holding = dict(zip(headers, row))
-    #         # holding = (row[0], int(row[1]), float(row[2]))
-    #         stock = {
-    #             'name': holding['name'], 
-    #             'shares': int(holding['shares']), 
-    #             'price': float(holding['price'])
-    #         }
-    #         portfolio.append(stock)
     portfolio = parse_csv(filename, select=['name', 'shares', 'price'], types=[str, int, float], has_headers=True)
 
     return portfolio
 
-# def read_portfolio_dict(filename):
-#     ''' Read a portfolio csv file and returns data as a list of dictionaries '''
-
-#     protfolio = []
-#     with open(filename, 'rt') as f:
-#         rows = csv.reader(f)
-#         headers = next(rows)
-#         for row in rows:
-#             holding = {'stock': row[0], 'shares': int(row[1]), 'price': float(row[2])}
-#             protfolio.append(holding)
-#     return protfolio
-
 def read_prices(filename):
     ''' reads a set of prices from csv file and generates dictionary '''
-    # prices = {}
-    # with open(filename, 'rt') as f:
-    #     rows = csv.reader(f)
-    #     for row in rows:
-    #         if row != []:
-    #             prices[row[0]] = float(row[1])
     prices = parse_csv(filename, types=[str,float], has_headers=False)
     return dict(prices)
 
@@ -80,26 +48,6 @@ def print_report(report):
         currency = f'${r[2]:.2f}'
         print(f'{r[0]:>10s} {r[1]:>10d} {currency:>10s} {r[3]:>10.2f}')    
 
-
-# print(read_portfolio('Data/portfolio.csv'))
-# print(read_portfolio_dict('Data/portfolio.csv'))
-# print(read_prices('Data/prices.csv'))
-
-# portfolio = read_portfolio('Data/portfolio.csv')
-
-
-# from collections import Counter
-# total_shares = Counter()
-# for item in portfolio:
-#     total_shares[item['name']] += item['shares']
-
-# print(total_shares['IBM'])
-
-# from collections import defaultdict
-# holdings=defaultdict(list)
-# for item in portfolio:
-#     holdings[item['name']].append((item['shares'], item['price']))
-# print(holdings['IBM'])
 
 def portfolio_report(portfolio_filename, prices_filename):
     portfolio = read_portfolio(portfolio_filename)
