e690223f72022ba9ddb743b3a36fbe2175877e19
Dan Bernards <dabernards@gmail.com>
2023-08-16 12:18:03 -0700

    Run through error handling

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,10 +3,14 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=","):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=",", silence_errors=False):
     '''
     Parse a CSV file into a list of records (provided version)
     '''
+
+    if select and not has_headers:
+        raise RuntimeError("passing 'select' argument requires headers")
+
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
 
@@ -14,20 +18,25 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=","
         if has_headers:
             headers = next(rows)
         records = []
-        for row in rows:
+        for i, row in enumerate(rows):
             if not row:    # Skip rows with no data
                 continue
-            if select:
-                # make sure select is in the right order
-                row = [item for item,head in zip(row, headers) if head in select]
-                headers = [head for head in headers if head in select]
-            if types:
-                row = [func(item) for item, func in zip(row, types)]
+            try:
+                if select:
+                    # make sure select is in the right order
+                    row = [item for item,head in zip(row, headers) if head in select]
+                    headers = [head for head in headers if head in select]
+                if types:
+                    row = [func(item) for item, func in zip(row, types)]
 
-            if has_headers:
-                record = {head: item for item, head in zip(row, headers)}
-            else:
-                record = tuple(row)
+                if has_headers:
+                    record = {head: item for item, head in zip(row, headers)}
+                else:
+                    record = tuple(row)
+                records.append(record)
+            except Exception as e:
+                if not silence_errors:
+                    print(f"Row {i+1:d}: Couldn't convert {row}")
+                    print(f"Row {i+1:d}: Reason {e}")
 
-            records.append(record)
     return records
