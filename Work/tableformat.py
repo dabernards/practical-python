d3e772253178e86abc76878196c63a90fd3ef055
Dan Bernards <github@dbernards.com>
2023-08-23 19:59:36 -0700

    getattr and __repr__ use instances

--- Work/tableformat.py
+++ Work/tableformat.py
@@ -11,6 +11,14 @@ def create_formatter(fmt):
     else:
         raise RuntimeError(f'Unknown format {fmt}')
 
+def print_table(portfolio, select, formatter):
+    ''' print arbitrary table '''
+    formatter.headings(select)
+    for s in portfolio:
+        for item in [getattr(s,col) for col in select]:
+            print(f'{str(item):>10s} ', end="")
+        print()
+
 
 class TableFormatter:
     def headings(self, headers):
